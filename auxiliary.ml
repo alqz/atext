16ca89dca3fb146cea238fea5b954df6650c7d90
Albert Zhang <alqz@outlook.com>
2015-12-02 20:14:49 -0500

    Bug fixed

--- auxiliary.ml
+++ auxiliary.ml
@@ -1,11 +1,15 @@
 let pd (s : string) : unit = print_endline ("[D> " ^ s ^ "]")
 let pdx (whether : bool) (s : string) : unit =
   if whether then print_endline ("[D> " ^ s ^ "]") else ()
-
 let pdi (il : int list) : unit =
   let istring : string = List.fold_left (fun acc i ->
       acc ^ (string_of_int i) ^ "; ") "" il in
   pd istring
+
+(* Uncomment these to no print. *)
+let pd (s : string) : unit = ()
+let pdx (whether : bool) (s : string) : unit = ()
+let pdi (il : int list) : unit = ()
 
 let fstop () = Pervasives.exit 0
 
