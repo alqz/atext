e980c77cca4bb57c0ab3c69455f9cebffb8e843a
Albert Zhang <alqz@outlook.com>
2015-12-05 20:31:23 -0500

    Bug fixed

--- writer.mli
+++ writer.mli
@@ -5,6 +5,9 @@
 
 (* This is the top level module and contains the main loop. *)
 
+(* Starts the program.
+ * Should never be called outside this module.
+ * This is only here for referece. *)
 val uncap : string list -> unit
 
 (* Exits cleanly. *)
