f959ecd42c327c0dc7ec4a5c93e29f2e8fa8d825
Albert Zhang <alqz@outlook.com>
2015-12-07 15:00:34 -0500

    Removed old tests

--- client_test.ml
+++
@@ -1,8 +0,0 @@
-open Server
-open Async.Std
-
-let _ =
-  init_client "localhost" 8080 >>= fun _ ->
-  return (print_endline "client connected!")
-
-let _ = Scheduler.go()
--- server_test.ml
+++
@@ -1,8 +0,0 @@
-open Server
-open Async.Std
-
-let _ =
-  init_server 8080 1 >>= fun _ ->
-  return (print_endline "server started!")
-
-let _ = Scheduler.go()
